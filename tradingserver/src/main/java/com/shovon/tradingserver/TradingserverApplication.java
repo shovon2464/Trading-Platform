a9482e6224477c95d67c538cf7ec372b2b33ebab
shovon2464 <shovon2464@gmail.com>
2026-05-06 22:15:08 -0600

    Spring server has been initialized

---
+++ tradingserver/src/main/java/com/shovon/tradingserver/TradingserverApplication.java
@@ -0,0 +1,13 @@
+package com.shovon.tradingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingserverApplication.class, args);
+	}
+
+}
---
+++ tradingserver/src/test/java/com/shovon/tradingserver/TradingserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shovon.tradingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
