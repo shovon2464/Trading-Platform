7e4dc2853d27a14e19f56e5d0f02f479aa8d2d42
shovon2464 <shovon2464@gmail.com>
2026-05-11 02:06:50 -0600

    Updated the allowed requestMatchers

---
+++ tradingserver/src/main/java/com/shovon/tradingserver/security/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.shovon.tradingserver.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Autowired
+  private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  @Value("app.env")
+  private String appEnv;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http,
+      CorsConfigurationSource corsConfigurationSource) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+        .authorizeHttpRequests(
+            (authorize) -> {
+              authorize
+                  .requestMatchers("/api/v1/users/create-account", "/api/v1/users/login")
+                  .permitAll()
+                  .anyRequest()
+                  .authenticated();
+            }
+        )
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .rememberMe(Customizer.withDefaults());
+    http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("http://localhost:8081");
+    config.addAllowedOrigin("http://localhost:8082");
+    config.addAllowedOrigin("http://localhost:8084");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+
+}
