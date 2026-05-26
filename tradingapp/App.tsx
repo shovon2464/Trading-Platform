4353b66495283f14e1e5e2e64b2d934a0673d0a5
shovon2464 <shovon2464@gmail.com>
2026-05-26 01:20:51 -0600

    Added back tradingapp

---
+++ tradingapp/App.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Trading App</Text>
+    </View>
+  );
+}
+
+export default App;
---
+++ tradingapp/src/constants/Colors.tsx
@@ -0,0 +1,27 @@
+
+
+export enum Colors {
+  themeColor = "#0EA5E9", 
+  
+ 
+  background = "#0F172A", 
+  background_light = "#1E293B", 
+  text = "#F8FAFC", 
+  text_light = "#E2E8F0",
+  border = "#475569",
+  card = "#334155", 
+  sub_background = "#1E293B", 
+  
+  active_tab = "#06B6D4", 
+  unactive_tab = "#64748B",
+  
+  notification_card = "#1E293B", 
+  
+  profit = "#16A34A",
+  loss = "#DC2626",
+  errorColor = "#F59E0B",
+  dark_background_light = "#888",
+  dark_text = "#111",
+  light_text = "#fff",
+  light_border = "#444",
+}
