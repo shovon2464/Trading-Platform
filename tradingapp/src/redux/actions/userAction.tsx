3cdb688f8169bacdc22ec5372d1199301ac90b9c
shovon2464 <shovon2464@gmail.com>
2026-06-03 21:44:56 -0600

    Created the OTP screen

--- tradingapp/src/redux/actions/userAction.tsx
+++ tradingapp/src/redux/actions/userAction.tsx
@@ -1,7 +1,7 @@
 import { appAxios } from '../../connectors/apiConfig.tsx';
 import { setUser } from '../reducers/userSlice.tsx';
 import { navigate, resetAndNavigate } from '../../utils/NavigationUtil.tsx';
-import { CHECK_EMAIL, EMAIL_LOGIN } from '../../connectors/API.tsx';
+import { CHECK_EMAIL, EMAIL_LOGIN, SEND_OTP, VERIFY_OTP } from '../../connectors/API.tsx';
 import axios from 'axios';
 import Toast from 'react-native-toast-message';
 import { token_storage } from '../storage.tsx';
@@ -57,7 +57,7 @@ export const emailLogin = (data: EmailLogin) => async (dispatch: any) => {
     token_storage.set("app_refresh_token", res.data.refreshToken);
     const { id, email, phoneExist, fullName } = res.data;
     await dispatch(setUser({ id, email, phoneExist, fullName }));
-    resetAndNavigate('AuthVerificationScreen');
+    resetAndNavigate('PersonalDetailScreen');
   } catch (error: any) {
     Toast.show({
       type: "normalToast",
@@ -66,6 +66,61 @@ export const emailLogin = (data: EmailLogin) => async (dispatch: any) => {
       }
     })
     console.log("EMAIL LOGIN ERROR-->", error);
+  }
+}
+
+
+interface SendOTP {
+  otp_type: string;
+  email: string;
+}
+
+export const sendOtp = (data: SendOTP) => async (dispatch: any)=> {
+  try {
+    const res = await axios.post(SEND_OTP, data);
+    console.log("SEND OTP-->", res.data);
+
+    Toast.show({
+      type: "normalToast",
+      props: {
+        msg: res?.data?.msg || "Something went wrong, please try again later"
+      }
+    });
+  } catch (error: any) {
+    Toast.show({
+      type: "normalToast",
+      props: {
+        msg: error?.response?.data?.msg || "Something went wrong, please try again later"
+      },
+    })
+    console.log("SEND OTP ERROR-->", error);
+  }
+}
+
+
+interface VerifyOTP {
+  otp_type: string;
+  email: string;
+  otp: string;
+  data?: string | null;
+}
+
+export const verifyOtp = (data: VerifyOTP) => async (dispatch: any) => {
+  try {
+    const res = await axios.post(VERIFY_OTP, data);
+    console.log("VERIFY OTP-->", res.data);
+
+    if (data.otp_type === "phone") {
+      resetAndNavigate("PersonalDetailScreen");
+    }
+  } catch (error: any) {
+    Toast.show({
+      type: "normalToast",
+      props: {
+        msg: error?.response?.data?.msg || "Something went wrong, please try again later"
+      },
+    });
+    console.log("VERIFY OTP ERROR-->", error);
   }
 }
 
--- tradingapp/src/screens/authentication/PhoneScreen.tsx
+++ tradingapp/src/screens/authentication/PhoneScreen.tsx
@@ -1,12 +1,255 @@
-import { Text, View } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
+import CustomText from '../../components/global/CustomText';
+import CustomSafeAreaView from '../../components/global/CustomSafeAreaView';
+import { FONTS } from '../../constants/Fonts';
+import { useTheme } from '@react-navigation/native';
+import { useAppDispatch, useAppSelector } from '../../redux/reduxHook';
+import { selectUser } from '../../redux/reducers/userSlice';
+import { SendOTP, VerifyOTP } from '../../redux/actions/userAction';
+import { Colors } from '../../constants/Colors';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import OtpTimer from '../../components/authentication/OtpTimer';
+import CustomButton from '../../components/global/CustomButton';
 
 const PhoneScreen = () => {
+  const { colors } = useTheme();
+  const user = useAppSelector(selectUser);
+  const dispatch = useAppDispatch();
+
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [otpSent, setOtpSent] = useState(false);
+  const [otp, setOtp] = useState('');
+  const [otpError, setOtpError] = useState('');
+
+  const handleSendOTP = async () => {
+    setLoading(true);
+    await dispatch(SendOTP({ email: user.email || '', otp_type: 'phone' }));
+    setOtpSent(true);
+    setLoading(false);
+  };
+
+  const handleVerifyOTP = async () => {
+    if (!otp) {
+      setOtpError('Enter OTP');
+      return;
+    }
+    setLoading(true);
+    await dispatch(
+      VerifyOTP({
+        email: user.email || '',
+        otp_type: 'phone',
+        data: phoneNumber,
+        otp: otp,
+      }),
+    );
+    setLoading(false);
+  };
+
+  const handlePress = async () => {
+    if (otpSent) {
+      handleVerifyOTP();
+      return;
+    }
+    handleSendOTP();
+  };
+
   return (
-    <View>
-      <Text>PhoneScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={10}
+      behavior="padding"
+      style={styles.keyboardContainer}
+    >
+      <CustomSafeAreaView>
+        <CustomText
+          variant="h4"
+          fontFamily={FONTS.Medium}
+          style={styles.mainContainer}
+        >
+          {otpSent ? 'Verify your mobile number' : 'Enter mobile number'}
+        </CustomText>
+
+        {otpSent ? (
+          <View style={styles.numberContainer}>
+            <CustomText variant="h8">
+              Enter the OTP sent to +91 {phoneNumber}
+            </CustomText>
+            <Icon
+              color={Colors.profit}
+              name="pencil"
+              size={RFValue(12)}
+              onPress={() => setOtpSent(false)}
+            />
+          </View>
+        ) : (
+          <CustomText variant="h8">
+            Mobile number is required to invest in India
+          </CustomText>
+        )}
+
+        {!otpSent ? (
+          <View style={styles.phoneContainer}>
+            <CustomText
+              variant="h5"
+              fontFamily={FONTS.NumberSemiBold}
+              style={{ fontWeight: 'bold' }}
+            >
+              +91
+            </CustomText>
+            <TextInput
+              focusable={true}
+              autoFocus={true}
+              keyboardType="phone-pad"
+              placeholder="9999999999"
+              maxLength={10}
+              style={[{ color: colors.text }, styles.textInput]}
+              value={phoneNumber}
+              onChangeText={text => {
+                setPhoneNumber(text);
+                setOtpError('');
+              }}
+            />
+          </View>
+        ) : (
+          <>
+            <View style={styles.phoneContainer}>
+              <TextInput
+                focusable={true}
+                autoFocus={true}
+                keyboardType="phone-pad"
+                placeholder="OTP"
+                maxLength={6}
+                style={[
+                  styles.otpInput,
+                  {
+                    backgroundColor: colors.card,
+                  },
+                ]}
+                value={otp}
+                onChangeText={text => setOtp(text)}
+              />
+            </View>
+
+            <View style={styles.otpTimerContainer}>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: colors.card,
+                  padding: 8,
+                  borderRadius: 5,
+                }}
+              >
+                <OtpTimer
+                  style={{
+                    fontSize: RFValue(10),
+                    color: colors.text,
+                    opacity: 0.8,
+                    fontFamily: FONTS.Regular,
+                  }}
+                  type="OTP"
+                  onPress={() => handleSendOTP()}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: colors.card,
+                  padding: 8,
+                  borderRadius: 5,
+                }}
+              >
+                <CustomText
+                  style={{
+                    fontSize: RFValue(10),
+                    color: colors.text,
+                    opacity: 0.8,
+                    fontFamily: FONTS.Regular,
+                  }}
+                >
+                  Get OTP via call
+                </CustomText>
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+
+        <View style={styles.btnContainer}>
+          {otpError && (
+            <View style={styles.errorContainer}>
+              <CustomText variant="h7" fontFamily={FONTS.Medium}>
+                Wrong OTP, 2 attempts remaining
+              </CustomText>
+            </View>
+          )}
+          <CustomButton
+            text={otpSent ? 'VERIFY' : 'SEND OTP'}
+            onPress={handlePress}
+            loading={loading}
+            disabled={loading}
+          />
+        </View>
+      </CustomSafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  otpTimerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 30,
+    marginTop: 50,
+  },
+  errorContainer: {
+    backgroundColor: 'rgba(255, 0, 0, 0.2)',
+    padding: 10,
+    justifyContent: 'center',
+    borderRadius: 4,
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  btnContainer: {
+    justifyContent: 'flex-end',
+    flex: 1,
+  },
+  otpInput: {
+    width: '90%',
+    fontWeight: 'bold',
+    fontSize: RFValue(15),
+    color: '#fff',
+    height: 45,
+    padding: 10,
+  },
+  keyboardContainer: {
+    flex: 1,
+  },
+  mainContainer: {
+    marginVertical: 10,
+  },
+  numberContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+  },
+  textInput: {
+    width: '90%',
+    fontWeight: 'bold',
+    fontSize: RFValue(15),
+  },
+  phoneContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    marginTop: 30,
+    paddingLeft: 3,
+  },
+});
 
 export default PhoneScreen;
